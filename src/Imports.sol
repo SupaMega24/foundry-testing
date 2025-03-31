20869edba96b5b3f9cde6b18f6fc2b4d5cf2018f
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2025-03-31 23:25:40 +0900

    Update imports

--- src/Imports.sol
+++ src/Imports.sol
@@ -4,3 +4,7 @@ pragma solidity ^0.8.24;
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
 contract Token is ERC20("name", "symbol", 18) {}
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestOz is Ownable(msg.sender) {}
